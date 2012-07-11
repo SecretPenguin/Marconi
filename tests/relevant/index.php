e3e1a2a4ac55c8908474137db55cf7f6b7f93ad4
Jason Sawyer <jason@secretpenguin.com>
2012-07-11 10:55:25 -0500

    Updated where sprite can begin + worked with browser resize

--- tests/relevant/index.php
+++ tests/relevant/index.php
@@ -26,18 +26,31 @@
 <script src="jquery.backgroundpos.min.js"></script>
 <script>
 
+var sceneHeight = 5000;
+
 $('#screen').height($(window).height());
 var screenHeight = $('#screen').height();
-$('#walking').height(screenHeight + 5000);
+$('#walking').height(screenHeight + sceneHeight);
 var walkingHeight = $('#walking').height();
 var difHeight = (walkingHeight - screenHeight);
-var totalHeight = (walkingHeight + screenHeight);
+/* var totalHeight = (walkingHeight + screenHeight); */
+
+
+$(window).resize(function() {
+	$('#screen').height($(window).height());
+	var screenHeight = $('#screen').height();
+	$('#walking').height(screenHeight + sceneHeight);
+	var walkingHeight = $('#walking').height();
+	var difHeight = (walkingHeight - screenHeight);
+	/* var totalHeight = (walkingHeight + screenHeight); */
+});
 
 $(window).on('scroll', function () {
 	var scrollTop = $(window).scrollTop(),
 	walkingOffset = $('#walking').offset().top,
 	distance = -(walkingOffset - scrollTop);
-
+	
+	/* Keep scene still during scroll */
 	if ( (distance < 0) ) {
 		$('#screen').removeClass().addClass('stickTop');
 	} else if ( distance >= difHeight ) {
@@ -46,18 +59,6 @@ $(window).on('scroll', function () {
 		$('#screen').removeClass().addClass('fixTop');
 	}
 	
-/*	var base = -100; /* Account for base position and make positive */
-/*
-	var start = 1500;
-	var speed = 3;
-	var move = (scrollTop - start)/speed + base;
-	if ( scrollTop > start ) {
-		$('#projectile').css('right', move);
-	} else {
-		$('#projectile').css('right', '-50');
-	}
-*/
-
 	var animOrigin = 0; /* Element offset */
 	var animTrigger = 1;
 	var animSpeed = 2; /* Default Speed updates number at 1/2 rate of pixels scroll - keep full pixel movement to trigger sprites */
@@ -66,11 +67,11 @@ $(window).on('scroll', function () {
 		animUpdate = (distance - animTrigger)/animSpeed + animOrigin;
 	}
 	
-	var spriteSpeed = 10; /* Frames between sprite triggers */
+	var spriteSpeed = 10; /* Pixels scrolled between sprite triggers */
 	var spriteNum = 5; /* Number of sprites in animation */
-	var spriteUpdate;
+	var spriteUpdate = 0;
 	function mySprite() {
-		spriteUpdate = Math.ceil((distance/spriteSpeed)%spriteNum); /* Example: at 250px scrolled 25 sprites will have switched and 5 complete animation cycles will have occurred */
+		spriteUpdate = Math.abs((Math.ceil(distance/spriteSpeed))%spriteNum); /* Example: at 250px scrolled 25 frames will have switched and 5 complete animation cycles will have occurred */
 	}
 	
 	if ( distance >= 500 ) {
@@ -94,28 +95,28 @@ $(window).on('scroll', function () {
 		$('#person').css('left', '75px');
 	}
 	
-	if ( (distance <= totalHeight) && (distance >= -(screenHeight)) ) {
+	if ( (distance <= walkingHeight) && (distance >= -(screenHeight)) ) {
 		spriteSpeed = 100;
 		spriteNum = 6;
 		mySprite();
-		if ( spriteUpdate == 1 ) {
+		if ( spriteUpdate == 0 ) {
 			$('#person').removeClass().addClass('f0');
-		} else if ( spriteUpdate == 2 ) {
+		} else if ( spriteUpdate == 1 ) {
 			$('#person').removeClass().addClass('f1');
-		} else if ( spriteUpdate == 3 ) {
+		} else if ( spriteUpdate == 2 ) {
 			$('#person').removeClass().addClass('f2');
-		} else if ( spriteUpdate == 4 ) {
+		} else if ( spriteUpdate == 3 ) {
 			$('#person').removeClass().addClass('f3');
-		} else if ( spriteUpdate == 5 ) {
+		} else if ( spriteUpdate == 4 ) {
 			$('#person').removeClass().addClass('f4');
-		} else if ( spriteUpdate == 6 ) {
+		} else if ( spriteUpdate == 5 ) {
 			$('#person').removeClass().addClass('f5');
 		}
 	}
 
 	
 /*
-	if ( (distance <= totalHeight) && (distance >= -(screenHeight)) ) {
+	if ( (distance <= walkingHeight) && (distance >= -(screenHeight)) ) {
 		spriteSpeed = 100;
 		mySprite();
 		console.log(spriteUpdate);
