7b5e849defb04eca8f2f886d77d9b92f509bee83
Jerod Santo <jerod.santo@gmail.com>
2012-09-06 14:18:19 -0500

    first pass at refactoring mobile section's javascript

---
+++ tests/mobile/mobile.js
@@ -0,0 +1,97 @@
+(function($) {
+  this.mobile = {
+    init: function() {
+      this.scrollBuffer = 400; // fit the scene to the screen
+      this.$screen = $("#screen");
+      this.$mobile = $("#mobile");
+      this.$devices = $("#devices");
+      this.$switchButtons = $("#switchboard a");
+      this.$devices = $("#devices > div");
+      this.$slidersContainer = $("#sliders");
+      this.$phoneSlider = $("#phoneSlider").remove();
+      this.$tabletSlider = $("#tabletSlider").remove();
+      this.$desktopSlider = $("#desktopSlider").remove();
+
+      $(window).on("resize", $.proxy(this.onResize, this));
+      $(window).on("scroll", $.proxy(this.onScroll, this));
+      this.$switchButtons.on("click", $.proxy(this.selectDevice, this));
+
+      // trigger a resize to set the initial screen and scene heights
+      $(window).resize();
+      this.activateSlider("phone");
+    },
+    // recalculate the screen height and scene height, then set the heights
+    // on the matching divs using new values
+    onResize: function(event) {
+      this.screenHeight = $(window).height();
+      this.sceneHeight = this.screenHeight + this.scrollBuffer;
+      this.$screen.height(this.screenHeight);
+      this.$mobile.height(this.sceneHeight);
+    },
+
+    onScroll: function(event) {
+      var scrollTop = $(window).scrollTop();
+      var mobileOffset = this.$mobile.offset().top;
+      var distance = -(mobileOffset - scrollTop);
+
+      // Keep scene still during scroll
+      if (distance < 0) {
+        this.$screen.attr("class", "stickTop");
+      } else if (distance >= this.scrollBuffer) {
+        this.$screen.attr("class", "stickBot");
+      } else if (distance >= 0 && distance < this.sceneHeight) {
+        this.$screen.attr("class", "fixTop");
+      }
+
+      // Animate current device in when approaching scene
+      var deviceSpeed = (distance / 2) + 146;
+      if (distance < -292) {
+        this.$devices.css("bottom", 0);
+      } else if ( distance >= -292 && distance < 0 ) {
+        this.$devices.css("bottom", deviceSpeed);
+      } else if (distance >= 0) {
+        this.$devices.css("bottom", 146);
+      }
+
+      // Start Sliders (if we decide to use auto play)
+      if ((distance >= -this.screenHeight) && ( distance <= this.sceneHeight)) {
+        // Trigger slider and turn off when outside scene
+        // Set correct slider to current based on current active nav element
+      }
+
+      console.log(distance);
+    },
+    selectDevice: function(event) {
+      var $clicked = $(event.target);
+      var thisEQ = $clicked.index();
+      var $selected = this.$devices.eq(thisEQ);
+      var selectedId = $selected.attr("id");
+
+      this.$switchButtons.removeClass("current");
+      $clicked.addClass("current");
+
+      this.$devices.stop().css("z-index", 0).animate({top: 231}, 500, function() {
+        $selected.stop().css("z-index", 10).animate({top: 0}, 500);
+      });
+
+      this.activateSlider(selectedId);
+    },
+
+    activateSlider: function(deviceId) {
+      var slider = this["$"+deviceId+"Slider"].clone();
+      this.$slidersContainer.animate({opacity: 0}, 800);
+      this.$slidersContainer.html(slider);
+      this.$slidersContainer.find("div").maximage({
+        cycleOptions: {
+          fx: "fade",
+          speed: 800,
+          timeout: 1000
+        },
+        cssBackgroundSize: false,
+        cssTransitions: false
+      });
+      this.$slidersContainer.animate({opacity: 1}, 800);
+    }
+  };
+  mobile.init();
+})(jQuery);
