751cc786cb7ea95afefefe3c16205c95fe038444
Jason Sawyer <jason@secretpenguin.com>
2012-09-19 14:00:44 -0500

    Rebuilt Flexible test

--- tests/flexible/index.php
+++ tests/flexible/index.php
@@ -11,132 +11,153 @@
 </head>
 <body>
 
-Notes:<br/>
-Month Range - Display the next set of months starting at the current month<br/>
-Credits - Possibly run some simple math to give people an idea of credit hours per month<br/>
-<br/>
-Resources:<br/>
-<a href="http://jqueryui.com/demos/slider/">http://jqueryui.com/demos/slider/</a><br/>
-<br/>
-
-<ul id="months">
-<li><a href="javascript:void(0);">Jan</a></li>
-<li><a href="javascript:void(0);">Feb</a></li>
-<li><a href="javascript:void(0);">Mar</a></li>
-<li><a href="javascript:void(0);">Apr</a></li>
-<li><a href="javascript:void(0);">May</a></li>
-<li><a href="javascript:void(0);">Jun</a></li>
-<li><a href="javascript:void(0);">Jul</a></li>
-<li><a href="javascript:void(0);">Aug</a></li>
-<li><a href="javascript:void(0);">Sep</a></li>
-<li><a href="javascript:void(0);">Oct</a></li>
-<li><a href="javascript:void(0);">Nov</a></li>
-<li><a href="javascript:void(0);">Dec</a></li>
-</ul>
-
-<p>Start Month = <span id="startMonth">January</span></p>
-<p>End Month = <span id="endMonth">July</span></p>
-<p>Duration = <span id="durYears">1 Year</span> <span id="durMonths">6 Months</span></p>
-<div id="flexible-container">
-	<div id="credits">
-		<div>&copy;</div>
-		<div>&copy;</div>
-		<div>&copy;</div>
-		<div>&copy;</div>
-		<div>&copy;</div>
-		<div>&copy;</div>
-		<div>&copy;</div>
-		<div>&copy;</div>
-		<div>&copy;</div>
-		<div>&copy;</div>
+<div id="timeline">
+	<div id="fast">
+		<!-- Use this as drop down to select start month - Make first Month be the next calendar month -->
+		<select id="startMonth">
+			<option>January</option>
+			<option>February</option>
+			<option>March</option>
+			<option>April</option>
+			<option>May</option>
+			<option>June</option>
+			<option>July</option>
+			<option>August</option>
+			<option>September</option>
+			<option>October</option>
+			<option>November</option>
+			<option>December</option>
+		</select>
+		<!-- Make this bar wider if earlier end date is selected -->
+		<div class="bar"></div>
+		<a id="fastEnd" class="toggleEnd" href="javascript:void(0);"></a>
+		<a id="fastMonth" href="javascript:void(0);"></a>
 	</div>
-	<div id="range"></div>
+	<div id="normal" class="currentEnd">
+		<!-- Breaks will display a scrolling background over a 6 month session and update depending on the months during that session -->
+		<div id="breaks"></div>
+		<div class="bar"></div>
+		<a id="normalEnd" class="toggleEnd" href="javascript:void(0);"></a>
+		<a id="normalMonth" href="javascript:void(0);"></a>
+	</div>
+	<div id="animations">
+		<div id="January">January Animation!</div>
+		<div id="February">February Animation!</div>
+		<div id="March">March Animation!</div>
+		<div id="April">April Animation!</div>
+		<div id="May">May Animation!</div>
+		<div id="June">June Animation!</div>
+		<div id="July">July Animation!</div>
+		<div id="August">August Animation!</div>
+		<div id="September">September Animation!</div>
+		<div id="October">October Animation!</div>
+		<div id="November">November Animation!</div>
+		<div id="December">December Animation!</div>
+	</div>
+	<div class="clear"></div>
 </div>
-
-<div id="animate">
-	<div id="January">Janimation!</div>
-	<div id="February">Februamation!</div>
-	<div id="March">March Animation!</div>
-	<div id="April">April Animation!</div>
-	<div id="May">Maytion!</div>
-	<div id="June">June Animation!</div>
-	<div id="July">July Animation!</div>
-	<div id="August">August Animation!</div>
-	<div id="September">September Animation!</div>
-	<div id="October">October Animation!</div>
-	<div id="November">November Animation!</div>
-	<div id="December">December Animation!</div>
+<div id="taglines">
+	<div>This is where the taglines go.<br/>January Tagline</div>
+	<div>This is where the taglines go.<br/>February Tagline</div>
+	<div>This is where the taglines go.<br/>March Tagline</div>
+	<div>This is where the taglines go.<br/>April Tagline</div>
+	<div>This is where the taglines go.<br/>May Tagline</div>
+	<div>This is where the taglines go.<br/>June Tagline</div>
+	<div>This is where the taglines go.<br/>July Tagline</div>
+	<div>This is where the taglines go.<br/>August Tagline</div>
+	<div>This is where the taglines go.<br/>September Tagline</div>
+	<div>This is where the taglines go.<br/>October Tagline</div>
+	<div>This is where the taglines go.<br/>November Tagline</div>
+	<div>This is where the taglines go.<br/>December Tagline</div>
 </div>
 
 <script src="http://code.jquery.com/jquery-latest.js"></script>
-<script src="../../js/jquery-ui-1.8.22.custom.min.js"></script>
+<script src="jquery.backgroundpos.min.js"></script>
 <script>
 
 var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
 // Set Global Month Variables
-var curMonth = 0;
-var endMonth = 6;
+var	curMonth = 0;
+var endMonth = 5;
+var endMonth2 = 11;
 
-var $endMonthText = $('#endMonth');
+var $fastEndText = $('#fastMonth');
+var $normalEndText = $('#normalMonth');
 
-// Set Current Month and Minimum End Month 
-$('#months a').click(function(){
-	curMonth = $(this).parent().index();
-	$('#startMonth').html(months[curMonth]);
-	// Minimum End Month is 18 Months after Start
-	if (curMonth > 5) {
-		endMonth = (curMonth -6);
+// Trigger animation
+function loadMonthAssets() {
+	$('#animations div').hide();
+	$('#taglines div').hide();
+	if ($('#normal').hasClass('currentEnd')) {
+		$('#animations div').stop().delay('500').eq(endMonth2).fadeIn();
+		$('#taglines div').stop().delay('500').eq(endMonth2).fadeIn();
 	} else {
-		endMonth = (curMonth + 6);
+		$('#animations div').stop().delay('500').eq(endMonth).fadeIn();
+		$('#taglines div').stop().delay('500').eq(endMonth).fadeIn();
 	}
-	$endMonthText.html(months[endMonth]);
+}
+
+/*
+function callTagline() {
+	$('#taglines div').fadeOut('500');
+	if ($('#normal').hasClass('currentEnd')) {
+		$('taglines div').stop().delay('500').eq(endMonth2).fadeIn();
+	} else {
+		$('taglines div').stop().delay('500').eq(endMonth).fadeIn();
+	}
+}
+*/
+
+// Stuff to do on initial load
+loadMonthAssets();
+$fastEndText.html('End of ' + months[endMonth]);
+$normalEndText.html('End of ' + months[endMonth2]);
+
+// Update months on change
+$('#startMonth').change(function(){
+	curMonth = $('#startMonth option:selected').index();
+	
+	// fast month
+	if (curMonth > 6) {
+		endMonth = (curMonth - 7);
+	} else {
+		endMonth = (curMonth + 5);
+	}
+	// normal month
+	if (curMonth == 0) {
+		endMonth2 = (curMonth + 11);
+	} else {
+		endMonth2 = (curMonth - 1);
+	}
+
+	$fastEndText.html('End of ' + months[endMonth]);	
+	$normalEndText.html('End of ' + months[endMonth2]);
+	
+	// animate break background
+	var updateBreak = -curMonth*80;
+	$('#breaks').animate({backgroundPosition: updateBreak + 'px center'}, 500);
+	
+	// Update sprite
+	loadMonthAssets();
 });
 
-// Range Slider set to 18 month range
-$('#range').slider({
-	range: 'min',
-	value: 1,
-	min: 1,
-	max: 18,
-	slide: function( event, ui ) {
-		// Start Date Range at 0
-		var dateOffset = ui.value - 1;
-		// Update End Month
-		var newEndMonth = (endMonth + dateOffset)%12;
-		$endMonthText.html(months[newEndMonth]);
-		
-		// Set Total Time to complete program
-		// Years
-		var durYears = Math.floor((18 + dateOffset)/12);
-		var $totalYears = $('#durYears');
-		if (durYears < 2) {
-			$totalYears.html(durYears+' Year');
-		} else {
-			$totalYears.html(durYears+' Years');
-		}
-		// Months
-		var durMonths = ((18 + dateOffset)%12);
-		var $totalMonths = $('#durMonths');
-		if (durMonths > 1) {
-			$totalMonths.html(durMonths+' Months');
-		} else if (durMonths == 1) {
-			$totalMonths.html(durMonths+' Month');
-		} else if (durMonths == 0) {
-			$totalMonths.html('');
-		}
-		
-		// Set CSS width of Credits to expand with slider
-		var creditWidth = 263 + (16 * dateOffset);
-		$('#credits').css('width', creditWidth);
-	},
-	stop: function( event, ui ) {
-		// Trigger Animation
-		var monthID = $endMonthText.text();
-		$('#animate div').hide();
-		$('#'+monthID).fadeIn();
-	}
+// Display normal timeline
+$('#normalEnd, #normalMonth').click(function(){
+	$('#normal').animate({opacity: 1}, 500).addClass('currentEnd');
+	$('#fast .bar').animate({height: '3px', bottom: '75px'}, 300);
+	$('#animations').css('right', '0');
+	loadMonthAssets();
 });
+
+// Display fast timeline
+$('#fastEnd, #fastMonth').click(function(){
+	$('#normal').animate({opacity: 0.35}, 500).removeClass();
+	$('#fast .bar').animate({height: '7px', bottom: '73px'}, 300);
+	$('#animations').css('right', '480px');
+	loadMonthAssets();
+});
+
 </script>
 
 </body>
