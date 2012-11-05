165335db0dc7c1420f5cb209c1caa82fe3231352
Jason Sawyer <jason@secretpenguin.com>
2012-11-05 10:11:25 -0600

    Updated skills in walking scene and removed test images.

--- scenes/walking.php
+++ scenes/walking.php
@@ -7,6 +7,7 @@
       <p><a class="scroll" href="javascript:void(0);">Play</a></p>
     </div>
     <div class="center">
+      <div class="tagline">Develop the skills that make you indispensable.</div>
       <div id="bear"></div>
       <div id="wall"></div>
       <div id="projectile"></div>
