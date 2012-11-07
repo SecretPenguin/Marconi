110375ae9e488a898c3a5872a454262b8139816b
Jason Sawyer <jason@secretpenguin.com>
2012-11-07 16:29:11 -0600

    Updated preface functions/design.

--- js/preface.js
+++ js/preface.js
@@ -2,28 +2,53 @@
   var preface = new Scene("preface", 2000);
 
   preface.init = function() {
-  	this.$slider = $('#intro');
+    this.$intro = $('#intro');
+  	this.$slider = $('#slider');
   	this.$title = $('h1');
+  	this.$purpleBG = $('#purpleBG');
+  	this.$preface = $('#preface');
+  	this.$scroll = $('#scrollDown');
 
     this.startSlider();
   };
 
   preface.onScroll = function(event) {
     this.conditionallyFixateScene();
-
-    if (this.distance <= 100) {
-      this.$slider.css('opacity', 1);
-    } else if (this.distance > 100 && this.distance <= 900) {
-      this.$slider.css('opacity', ((200 - (this.distance - 100)/4)/200));
-    } else if (this.distance > 900) {
-      this.$slider.css('opacity', 0);
+    
+    
+    if (this.distance <= 500) {
+      this.$scroll.delay('5000').queue(function() {
+        $(this).addClass('showScroll');
+        $(this).dequeue();
+      });
+    } else if (this.distance > 500) {
+      this.$scroll.removeClass('showScroll');
     }
 
-    if (this.distance <= 1000) {
+    if (this.distance <= 100) {
+      this.$intro.css('opacity', 1);
+    } else if (this.distance > 100 && this.distance <= 900) {
+      this.$intro.css('opacity', ((800 - (this.distance - 100))/800));
+    } else if (this.distance > 900) {
+      this.$intro.css('opacity', 0);
+    }
+    
+    if (this.distance <= 500) {
+      this.$purpleBG.css('opacity', 1);
+      this.$preface.css('background-color', '#1c1422');
+    } else if (this.distance > 500 && this.distance <= 1000) {
+      this.$purpleBG.css('opacity', (500 - (this.distance - 500))/500);
+      this.$preface.css('background-color', '#fff');
+    } else if (this.distance >= 1000) {
+      this.$purpleBG.css('opacity', 0);
+      this.$preface.css('background-color', '#fff');
+    }
+
+    if (this.distance <= 900) {
       this.$title.css('opacity', 0);
-    } else if (this.distance > 1000 && this.distance < 1400) {
-   	  this.$title.css('opacity', ((this.distance - 1000)/400));
-    } else if (this.distance >= 1400) {
+    } else if (this.distance > 900 && this.distance < 1300) {
+   	  this.$title.css('opacity', ((this.distance - 900)/400));
+    } else if (this.distance >= 1300) {
       this.$title.css('opacity', 1);
     }
   };
@@ -31,13 +56,18 @@
   preface.startSlider = function() {
     this.$slider.cycle({
       fx:'fade',
-      speed: 800,
+      speedOut: 300,
+      speedIn: 1100,
       timeout:0,
       next: '#intro p',
       startingSlide: Math.floor(Math.random()*4),
-      cleartypeNoBg: true
+      cleartypeNoBg: true,
+      pager: '#slider-nav',
+      pagerAnchorBuilder: function paginate(idx, el) {
+			return '#slider-nav li:eq(' + idx + ') a';
+			}
     });
   };
-
+  
   this.M.register(preface);
 })(jQuery);
--- scenes/preface.php
+++ scenes/preface.php
@@ -1,11 +1,23 @@
 <div id="preface">
 	<div id="preface-screen">
+		<div id="purpleBG"></div>
 		<div id="intro">
-			<p>Intro Tagline One</p>
-			<p>Intro Tagline Two</p>
-			<p>Intro Tagline Three</p>
-			<p>Intro Tagline Four</p>
+			<div id="preface-texture"></div>
+			<div id="logo">Bellevue University</div>
+			<div id="slider">
+				<p id="preface-tagline-1">Are you ready for a new way to learn?</p>
+				<p id="preface-tagline-2">Does your present stand in the way of your future?</p>
+				<p id="preface-tagline-3">What if there was nothing stopping you from getting your degree?</p>
+				<p id="preface-tagline-4">Can a degree give you real skills without the hassles?</p>
+			</div>
+			<ul id="slider-nav">
+				<li><a id="pager1" href="javascript:void(0);">1</a></li>
+				<li><a id="pager2" href="javascript:void(0);">2</a></li>
+				<li><a id="pager3" href="javascript:void(0);">3</a></li>
+				<li><a id="pager4" href="javascript:void(0);">4</a></li>
+			</ul>
 		</div>
+		<div id="scrollDown">Scroll Down</div>
 		<h1>Title + Logo</h1>
 	</div>
 </div>
