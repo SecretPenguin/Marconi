255f847c56a770065c29ed9d1222e5d1fa4281a9
Jason Sawyer <jason@secretpenguin.com>
2012-11-07 11:05:35 -0600

    Updated bird icon and some text for social.

--- scenes/social.php
+++ scenes/social.php
@@ -6,7 +6,7 @@
 				<div id="first-message" class="left dog">
 					<div class="bubble">
 						<div class="point"></div>
-						<div class="username">Dog <span>June 28th, 2012</span></div>
+						<div class="username">Mary <span>June 28th, 2012</span></div>
 						<p class="message">Did today’s Geology lecture making any sense to anyone?</p>
 					</div>
 				</div>
@@ -14,7 +14,7 @@
 				<div id="user-message-one" class="user-message">
 					<div class="bubble">
 						<div class="point"></div>
-						<div class="username">Cat <span>June 28th, 2012</span></div>
+						<div class="username">You <span>June 28th, 2012</span></div>
 						<p class="message">I thought it <em>rocked</em>.</p>
 					</div>
 				</div>
@@ -22,7 +22,7 @@
 				<div class="reply-to-user-message-one left bird">
 					<div class="bubble">
 						<div class="point"></div>
-						<div class="username">Bird <span>June 28th, 2012</span></div>
+						<div class="username">Jim <span>June 28th, 2012</span></div>
 						<p class="message">Yeah, I think Prof. Jennings is a real <em>gem</em>.</p>
 					</div>
 				</div>
@@ -30,7 +30,7 @@
 				<div class="reply-to-user-message-one left dog">
 					<div class="bubble">
 						<div class="point"></div>
-						<div class="username">Dog <span>June 28th, 2012</span></div>
+						<div class="username">Mary <span>June 28th, 2012</span></div>
 						<p class="message">OK funny guys, I’m <em>grounding</em> you from any more puns.</p>
 					</div>
 				</div>
@@ -38,7 +38,7 @@
 				<div id="user-message-two" class="user-message">
 					<div class="bubble">
 						<div class="point"></div>
-						<div class="username">Cat <span>June 28th, 2012</span></div>
+						<div class="username">You <span>June 28th, 2012</span></div>
 						<p class="message">Haha. We’re not trying to be <em>hard</em> on you.</p>
 					</div>
 				</div>
@@ -46,7 +46,7 @@
 				<div class="reply-to-user-message-two left bird">
 					<div class="bubble">
 						<div class="point"></div>
-						<div class="username">Bird <span>June 28th, 2012</span></div>
+						<div class="username">Jim <span>June 28th, 2012</span></div>
 						<p class="message">Yeah, no <em>dig</em> intended.</p>
 					</div>
 				</div>
@@ -54,7 +54,7 @@
 				<div class="reply-to-user-message-two left dog">
 					<div class="bubble">
 						<div class="point"></div>
-						<div class="username">Dog <span>June 28th, 2012</span></div>
+						<div class="username">Mary <span>June 28th, 2012</span></div>
 						<p class="message">Well, you really cut me to the <em>core</em> there.</p>
 					</div>
 				</div>
@@ -62,7 +62,7 @@
 				<div id="user-message-three" class="user-message">
 					<div class="bubble">
 						<div class="point"></div>
-						<div class="username">Cat <span>June 28th, 2012</span></div>
+						<div class="username">You <span>June 28th, 2012</span></div>
 						<p class="message">Chin up, I took that class last semester and I liked it a <em>lava</em>.</p>
 					</div>
 				</div>
@@ -70,7 +70,7 @@
 				<div class="reply-to-user-message-three left dog">
 					<div class="bubble">
 						<div class="point"></div>
-						<div class="username">Dog <span>June 28th, 2012</span></div>
+						<div class="username">Mary <span>June 28th, 2012</span></div>
 						<p class="message">You guys are such <em>ashes</em>.</p>
 					</div>
 				</div>
@@ -84,6 +84,6 @@
 			<input type="submit"/>
 		</form>
 		
-		<div id="disclaimer">The Geology class represented in this faux conversation is not currently offered by this program, but the functionality to converse with your classmates and alumn is. The animals represented here are fictional. You are not really a cat. Furthermore, animals are not allowed to sign up for this program. Sorry, Whiskers.</div>
+		<div id="disclaimer">The Geology class represented in this conversation is not currently offered by this program, but the functionality to converse with your classmates and alumn is. The animals represented here are fictional. You are not really a cat. Furthermore, animals are not allowed to sign up for this program. Sorry, Whiskers.</div>
 	</div>
 </div>
